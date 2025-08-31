94076984e0855685ae5a14105dba6749c0d1232b
Joderick Sherwin J <130847427+Joderick-Sherwin@users.noreply.github.com>
2025-08-31 12:44:41 +0530

    Update test_camera.py

--- tests/test_camera.py
+++ tests/test_camera.py
@@ -1,1 +1,10 @@
- 
+import unittest
+from camera.webcam import Webcam
+
+class TestWebcam(unittest.TestCase):
+    def test_capture_frame(self):
+        cam = Webcam(0)
+        frame = cam.read_frame()
+        self.assertIsNotNone(frame)
+        self.assertEqual(len(frame.shape), 3)  # Should be a color image
+        cam.release()
