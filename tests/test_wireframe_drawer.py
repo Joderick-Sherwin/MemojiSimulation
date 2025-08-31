734369360be3adbaa26d534b9d0974eec9d7246c
Joderick Sherwin J <130847427+Joderick-Sherwin@users.noreply.github.com>
2025-08-31 12:45:18 +0530

    Update test_wireframe_drawer.py

--- tests/test_wireframe_drawer.py
+++ tests/test_wireframe_drawer.py
@@ -1,1 +1,10 @@
- 
+import unittest
+import numpy as np
+from visualizers.wireframe_drawer import WireframeDrawer
+
+class TestWireframeDrawer(unittest.TestCase):
+    def test_draw(self):
+        drawer = WireframeDrawer()
+        dummy_frame = np.zeros((480, 640, 3), dtype=np.uint8)
+        result_frame = drawer.draw(dummy_frame, None)
+        self.assertEqual(result_frame.shape, dummy_frame.shape)
